6b3a83319c5b4cd71743d69dc9ae4cc70f8bbe1c
Frey <freyandhy@gmail.com>
2019-05-15 18:37:00 +0700

    Updates

---
+++ precache-manifest.735d4104c39973f497539872e6ade5f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e819e79cd4ad68e4c5dfd4774e6d793b",
+    "url": "/react-counter/index.html"
+  },
+  {
+    "revision": "6f6aeef7a5d49d603d8c",
+    "url": "/react-counter/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "1a9e6d0b435c1e176dd2",
+    "url": "/react-counter/static/css/main.8e365c4a.chunk.css"
+  },
+  {
+    "revision": "6f6aeef7a5d49d603d8c",
+    "url": "/react-counter/static/js/2.ee15027a.chunk.js"
+  },
+  {
+    "revision": "1a9e6d0b435c1e176dd2",
+    "url": "/react-counter/static/js/main.55999c94.chunk.js"
+  },
+  {
+    "revision": "2c73fdf03faab0d3c96b",
+    "url": "/react-counter/static/js/runtime~main.35208266.js"
+  }
+]);
